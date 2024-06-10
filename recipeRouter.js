8f2f8462641d4d75e9cb787a07ab38907de4a7d2
ethomas1750 <144511038+ethomas1750@users.noreply.github.com>
2024-06-09 20:10:28 -0500

    Update recipeRouter.js

--- recipeRouter.js
+++ recipeRouter.js
@@ -1,1 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const {getAllRecipe,createRecipe} = require("./controller/recipeController")
 
+router.get("/getall/recipe",(req,res)=>{
+    getAllRecipe()
+        .then((payload)=>{
+            res.json({message:"Success",data: payload})
+        })
+        .catch((error)=>{
+            res.json({message:"Error",error: error})
+        })
+})
+
+router.post("/create/new/recipe",(req,res)=>{
+    createRecipe(req.body)
+        .then((payload)=>{
+            res.json({message:"New Recipe Uploaded",data:payload})
+        })
+        .catch((error)=>{
+            res.json({message:"Error",error:error})
+        })
+})
+
+module.exports = router
